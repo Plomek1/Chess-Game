79b1a2a1c6d9b744a0144d9cd3124bf45db5cf5a
Plomek <d.cwierz1@gmail.com>
2025-02-05 20:40:48 +0100

    mouse input

--- Chess/Assets/Scripts/Input/MouseInput.cs
+++ Chess/Assets/Scripts/Input/MouseInput.cs
@@ -4,11 +4,84 @@ namespace Chess.Gameplay
 {
     public class MouseInput : MonoBehaviour
     {
+        private PlayerBoardSquare highlightedSquare;
         private PlayerBoardSquare selectedSquare;
+        private PlayerBoardSquare hoveredSquare;
+
+        private Transform lastHit;
+
 
         private void Update()
         {
+            RayForSquare();
 
+            //TODO: PIECE DRAGGING
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(selectedSquare)
+                {
+                    if (highlightedSquare)
+                    {
+                        Debug.Log("move");
+                    }
+                    Debug.Log("aha");
+                    DeselectSquare();
+                }
+
+                else SelectHighlightedSquare();
+            }
+        }
+
+        private void HighlightHoveredSquare()
+        {
+            highlightedSquare?.Deselect();
+            highlightedSquare = hoveredSquare;
+            highlightedSquare.Highlight();
+        }
+
+        private void SelectHighlightedSquare()
+        {
+            selectedSquare?.Deselect();
+            highlightedSquare?.Select();
+
+            selectedSquare = highlightedSquare;
+            highlightedSquare = null;
+        }
+
+        private void DeselectSquare()
+        {
+            if (!selectedSquare) return;
+
+            if (hoveredSquare == selectedSquare) selectedSquare.Highlight();
+            else selectedSquare.Deselect();
+
+            selectedSquare = null;
+        }
+
+        private void RayForSquare()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo) && hitInfo.transform.TryGetComponent(out PlayerBoardSquare square))
+            {
+                if (hitInfo.transform != lastHit)
+                {
+                    hoveredSquare = square;
+                    if(hoveredSquare == selectedSquare)
+                    {
+                        highlightedSquare?.Deselect();
+                        highlightedSquare = null;
+                    }
+                    else HighlightHoveredSquare();
+
+                    lastHit = hitInfo.transform;
+                }
+            }
+            else
+            {
+                highlightedSquare?.Deselect();
+                highlightedSquare = null;
+                lastHit = null;
+            }
         }
     }
 }
--- Chess/Assets/Scripts/Input/PlayerBoardSquare.cs
+++ Chess/Assets/Scripts/Input/PlayerBoardSquare.cs
@@ -9,15 +9,29 @@ namespace Chess.Gameplay
         Action<Spot> SquareSelected;
 
         [SerializeField]
-        private Material whiteMat;
+        private Material matIdleWhite;
         [SerializeField]
-        private Material blackMat;
+        private Material matIdleBlack;
+
+        [SerializeField]
+        private Material matHighlightedWhite;
+        [SerializeField]
+        private Material matHighlightedBlack;
+
+        [SerializeField]
+        private Material matSelectedWhite;
+        [SerializeField]
+        private Material matSelectedBlack;
 
         private Spot spot;
+        private bool isWhite;
+
+        private MeshRenderer meshRenderer;
 
         public void Init(Spot spot, bool isWhite)
         {
             this.spot = spot;
+            this.isWhite = isWhite;
             name = spot.notation;
 
             //Setting position
@@ -26,7 +40,22 @@ namespace Chess.Gameplay
             Vector3 pos = a1Pos + new Vector3(spot.file - 1, 0, spot.rank - 1) * size;
             transform.position = pos;
 
-            GetComponent<MeshRenderer>().material = isWhite? whiteMat : blackMat;
+            meshRenderer = GetComponent<MeshRenderer>();
+            meshRenderer.material = isWhite ? matIdleWhite : matIdleBlack;
+        }
+
+        public void Highlight()
+        {
+            meshRenderer.material = isWhite ? matHighlightedWhite : matHighlightedBlack;
+        }
+        public void Select()
+        {
+            meshRenderer.material = isWhite ? matSelectedWhite : matSelectedBlack;
+
+        }
+        public void Deselect()
+        {
+            meshRenderer.material = isWhite ? matIdleWhite : matIdleBlack;
         }
     }
 }
